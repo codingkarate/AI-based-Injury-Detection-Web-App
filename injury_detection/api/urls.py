fef495836e8a9779f6f7b998424e2b6f7443355d
Amit Bikram Mishra <bikramamitabm@gmail.com>
2025-09-26 18:28:05 +0530

    REST API endpoints created

--- injury_detection/api/urls.py
+++ injury_detection/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test_mongo/", views.test_mongo, name="test_mongo"),
+    path('test-mongo/', views.test_mongo, name='test_mongo'),
+    path('exercises/', views.exercises, name='exercises'),
 ]
--- injury_detection/api/views.py
+++ injury_detection/api/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .utils.db import users
 
+# Existing MongoDB test API
 @api_view(['GET'])
 def test_mongo(request):
     # Insert a sample user
@@ -13,4 +12,13 @@ def test_mongo(request):
     # Fetch data
     data = list(users.find({}, {"_id": 0}))  # exclude _id for JSON safety
     return JsonResponse({"users": data})
+
+# New Exercises API
+@api_view(['GET'])
+def exercises(request):
+    data = {
+        "exercises": ["Jumping Squats", "Lunges", "Pushups"]
+    }
+    return JsonResponse(data)
+
 
